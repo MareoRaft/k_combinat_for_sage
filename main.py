e8a797984e66d7143547cc61cb41291d1f998664
Matthew Lancellotti <mareoraft@gmail.com>
2018-05-24 21:49:39 -0400

    move skew partn methods together

--- main.py
+++ main.py
@@ -80,6 +80,62 @@ def row_col_to_skew_partition(rs, cs):
             num_rows_to_slide -= 1
     return SkewPartition([outer, inner])
 
+def bump_path_piece(sp, start_row_index, blocked_rows=set()):
+    # this algo find the correct "L" piece of the path, where the bottom right cell is cell1, the bottom left is cell2, and the top left is cell3
+    # Returns (top_row_index, is_end) which are the row index of cell3 and whether or not we 'broke free' out of the top left cell of the skew-partition, respectively.
+    col_index2 = left(sp, start_row_index)
+    row_index3 = top(sp, col_index2) + 1
+    while row_index3 in blocked_rows:
+        row_index3 += 1
+    # CATTY-CORNER ONLY line:
+    max_row_index = len(sp.outer()) - 1
+    if row_index3 > max_row_index:
+        return None, True
+    else:
+        return row_index3, False
+def bump_path(sp, row_index, blocked_rows=set()):
+    new_blocked_rows = {row_index}
+    # is_end says if you reached the end of the path
+    while True:
+        row_index, is_end = bump_path_piece(sp, row_index, blocked_rows)
+        if is_end:
+            break
+        else:
+            new_blocked_rows.add(row_index)
+    return new_blocked_rows
+def skew_partition_to_selected_rows(sp):
+    # actually this may ONLY WORK for catty-connected skew-partitions, because i'm not sure how we deal with 'missing' rows
+    # arguably we should call it a linked_skew_partition
+    # record the indices of rows that have been used up
+    selected_rows = set()
+    blocked_rows = set()
+    for row_index, outer_row in enumerate(sp.outer()):
+        if row_index not in blocked_rows:
+            selected_rows.add(row_index)
+            new_blocked_rows = bump_path(sp, row_index, blocked_rows)
+            blocked_rows.update(new_blocked_rows)
+    return sorted(selected_rows)
+def selected_rows_to_root_ideal(n, selected_indecis):
+    """Given the dimension of the square n and the selected rows, output the root ideal """
+    root_ideal_cells = []
+    selected_indecis = set(selected_indecis)
+    permitted_col_indecis = set(range(n)) - selected_indecis
+    for i in range(n):
+        if i in selected_indecis:
+            if permitted_col_indecis:
+                smallest_unblocked_index = min(permitted_col_indecis)
+                root_ideal_cells += [(i, j) for j in range(smallest_unblocked_index, n)]
+                permitted_col_indecis.remove(smallest_unblocked_index)
+                selected_indecis.add(smallest_unblocked_index)
+    return root_ideal_cells
+def skew_partition_to_root_ideal(sp):
+    selected_indecis = skew_partition_to_selected_rows(sp)
+    n = len(sp.outer())
+    root_ideal = selected_rows_to_root_ideal(n, selected_indecis)
+    return root_ideal
+
+def skew_partition_to_removable_roots(sp):
+    pass
 
 """
 Given a skew-linked diagram, is it a k-boundary?  (That is, does there exist some partition which - when cells of hook-length > k are removed - becomes the skew-linked diagram.)
@@ -108,6 +164,7 @@ def is_k_boundary(skew_shape, k):
         """now that we have the partition, we simply compute it's hook-length for each cell and verify that for each cell of values k or less, it appears in the skew_shape"""
         correct_k_boundary = l.k_boundary(k)
         return skew_shape == correct_k_boundary
+# End SkewPartition methods.
 
 
 # Partition methods:
@@ -194,6 +251,17 @@ def Partition_is_k_reducible(ptn, k):
 
 def Partition_is_k_irreducible(ptn, k):
     return not Partition_is_k_reducible(ptn, k)
+
+def is_symmetric(ptn):
+    """Given a partition λ, detect if λ = λ'.
+
+    This function runs in LINEAR time of order length(λ).
+    """
+    for j in range(0, len(ptn)):
+        for k in range(ptn[-j], ptn[-j-1]):
+            if ptn[k] != len(ptn) - j:
+                return False
+    return True
 # END Partition methods.
 
 def get_k_rectangles(k):
@@ -235,71 +303,6 @@ def n_to_number_of_linked_partition_self_pairs(n):
         else:
             count += 1
     return count
-
-def bump_path_piece(sp, start_row_index, blocked_rows=set()):
-    # this algo find the correct "L" piece of the path, where the bottom right cell is cell1, the bottom left is cell2, and the top left is cell3
-    # Returns (top_row_index, is_end) which are the row index of cell3 and whether or not we 'broke free' out of the top left cell of the skew-partition, respectively.
-    col_index2 = left(sp, start_row_index)
-    row_index3 = top(sp, col_index2) + 1
-    while row_index3 in blocked_rows:
-        row_index3 += 1
-    # CATTY-CORNER ONLY line:
-    max_row_index = len(sp.outer()) - 1
-    if row_index3 > max_row_index:
-        return None, True
-    else:
-        return row_index3, False
-def bump_path(sp, row_index, blocked_rows=set()):
-    new_blocked_rows = {row_index}
-    # is_end says if you reached the end of the path
-    while True:
-        row_index, is_end = bump_path_piece(sp, row_index, blocked_rows)
-        if is_end:
-            break
-        else:
-            new_blocked_rows.add(row_index)
-    return new_blocked_rows
-def skew_partition_to_selected_rows(sp):
-    # actually this may ONLY WORK for catty-connected skew-partitions, because i'm not sure how we deal with 'missing' rows
-    # arguably we should call it a linked_skew_partition
-    # record the indices of rows that have been used up
-    selected_rows = set()
-    blocked_rows = set()
-    for row_index, outer_row in enumerate(sp.outer()):
-        if row_index not in blocked_rows:
-            selected_rows.add(row_index)
-            new_blocked_rows = bump_path(sp, row_index, blocked_rows)
-            blocked_rows.update(new_blocked_rows)
-    return sorted(selected_rows)
-def selected_rows_to_root_ideal(n, selected_indecis):
-    """Given the dimension of the square n and the selected rows, output the root ideal """
-    root_ideal_cells = []
-    selected_indecis = set(selected_indecis)
-    permitted_col_indecis = set(range(n)) - selected_indecis
-    for i in range(n):
-        if i in selected_indecis:
-            if permitted_col_indecis:
-                smallest_unblocked_index = min(permitted_col_indecis)
-                root_ideal_cells += [(i, j) for j in range(smallest_unblocked_index, n)]
-                permitted_col_indecis.remove(smallest_unblocked_index)
-                selected_indecis.add(smallest_unblocked_index)
-    return root_ideal_cells
-def skew_partition_to_root_ideal(sp):
-    selected_indecis = skew_partition_to_selected_rows(sp)
-    n = len(sp.outer())
-    root_ideal = selected_rows_to_root_ideal(n, selected_indecis)
-    return root_ideal
-
-def is_symmetric(l):
-    """Given a partition l, detect if l = l'.
-
-    This function runs in LINEAR time of order length(l).
-    """
-    for j in range(0, len(l)):
-        for k in range(l[-j], l[-j-1]):
-            if l[k] != len(l) - j:
-                return False
-    return True
 
 def sequence(func, num_terms=20):
     seq = []
