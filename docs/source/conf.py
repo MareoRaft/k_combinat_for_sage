e7ef5552628e5cd9a88c5e810b01b56cb5759a3c
Matthew Lancellotti <mareoraft@gmail.com>
2018-06-06 07:32:54 -0400

    reverting

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -43,7 +43,7 @@ extensions = [
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['templates']
+templates_path = ['_templates']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
@@ -104,12 +104,12 @@ html_theme = 'sage'
 html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
-html_theme_path = ['themes']
+html_theme_path = ['_themes']
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['static']
+html_static_path = ['_static']
 
 
 # Custom sidebar templates, must be a dictionary that maps document names
