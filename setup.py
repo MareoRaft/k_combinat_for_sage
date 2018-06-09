5f3e480b9c5844af5007b030b153610a90add182
Matthew Lancellotti <mareoraft@gmail.com>
2018-06-09 16:38:51 -0400

    renamed src folder.  created setup files.

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+	name='morse_code',
+	packages=['morse_code'], # must be same as name above
+	version='0.0.1',
+	description='Code for Jennifer Morse to be used with SageMath',
+	author='Matthew Lancellotti',
+	author_email='mvlancellotti@gmail.com',
+	url='https://github.com/mareoraft/morse-code', # URL to github repo
+	download_url='https://github.com/mareoraft/morse-code/archive/0.0.1.tar.gz',
+	keywords=['morse', 'k-boundary', 'k-rim', 'k-shape', 'skew-linked diagram', 'k-irreducible', 'root ideals'], # arbitrary keywords
+	classifiers=[],
+)
