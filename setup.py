eb4e7a1abf5d11b1bb472cb2690e1ce44ef1421d
Matthew Lancellotti <mareoraft@gmail.com>
2018-07-13 13:41:37 -0400

    0.0.16

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import sys
 # config
 name = 'k_combinat_for_sage'
 # version = WILL BE AUTOMATICALLY POPULATED IN LINE BELOW.  DO NOT EDIT LINE BELOW.
-version = '0.0.15'
+version = '0.0.16'
 url = 'https://github.com/mareoraft/{name}'.format(name=name)
 download_url = '{url}/archive/{tag}.tar.gz'.format(url=url, tag=version)
 setup(
